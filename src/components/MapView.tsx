b33b1dbc6c31bb14b961bc5f3b790a4df2d218c6
bukthorn <unthouckseen@gmail.com>
2026-09-23 00:20:31 +0200

    Add files via upload

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
-import maplibregl from 'maplibre-gl'
+import * as maplibregl from 'maplibre-gl'
 import type { BenchItem } from '../types/bench'
 import { parseCoordinate } from '../utils/geo'
 import { getType, getTypeColor } from '../utils/tags'
